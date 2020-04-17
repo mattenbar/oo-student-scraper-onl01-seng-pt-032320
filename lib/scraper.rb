37c4c2d2ff262fb5a927039bfb90fbc70736060b
matthew enbar <matthew.enbar@gmail.com>
2020-04-17 17:14:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     students_array = []
     doc = Nokogiri::HTML(open(index_url))
-    doc.student_card.css(".student-card").each do |student|
+    doc.css(".student-card").each do |student|
       student_info = {}
       student_info[:name] = student.css("h4.student-name").text
       student_info[:location] = student.css("p.student-location").text
