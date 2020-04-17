b8256fe8f9363ad64b22ebed4cd5b29e227eef35
matthew enbar <matthew.enbar@gmail.com>
2020-04-17 15:13:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
+    doc.css(".student-card")
   end
 
   def self.scrape_profile_page(profile_url)
