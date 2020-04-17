704505a63cce23524af3687869a2e5eac8650139
matthew enbar <matthew.enbar@gmail.com>
2020-04-17 16:52:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,11 +8,14 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
+    doc.css(".roster-cards-container").each do |student_card|
+      student_card.css(".student-card").each do |student|
+        "#{student.attribute('href')}"
+      end
+    end
 
-    #student names - doc.css(".student-name").text
-    #student locations - doc.css(".student-location").text
-    #student URL -
-    binding.pry
+
+    #binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
