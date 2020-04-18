51d9dcdbaa851773b717a978efebe76b6d1eee52
matthew enbar <matthew.enbar@gmail.com>
2020-04-18 18:03:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{k}="), v)
+    end
   end
 
   def self.all
